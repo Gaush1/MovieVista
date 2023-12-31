629931681b6f9237f0f3589948fcbc3e89f16024
Gaush1 <gaush5u786@gmail.com>
2023-12-31 16:12:09 +0530

    Carousel section added.

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 function Trending() {
   const [endpoint, setEndPoint] = useState("day");
@@ -18,6 +19,7 @@ function Trending() {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading}/>
     </div>
   );
 }
