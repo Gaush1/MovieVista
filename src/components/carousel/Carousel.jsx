640d8e72805f6001d883f39adc82f73fa720aeae
Gaush1 <gaush5u786@gmail.com>
2024-01-01 18:05:18 +0530

    Carousel updated.

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,7 +15,7 @@ import Genres from "../genres/Genres";
 
 import "./style.scss";
 
-function Carousel({ data, loading }) {
+function Carousel({ data, loading,endpoint }) {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
@@ -66,7 +66,7 @@ function Carousel({ data, loading }) {
                 <div
                   key={item.id}
                   className="carouselItem"
-                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                  onClick={() => navigate(`/${item.media_type ? item.media_type : endpoint}/${item.id}`)}
                 >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
